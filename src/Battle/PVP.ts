d21e27bd12e6bc4df5a44a12fc4a45303995a69c
Lucas <lm.spizzirri@gmail.com>
2023-09-13 14:10:49 -0300

    Implementado o PvP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,18 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  constructor(private player1: Fighter, private player2: Fighter) { 
+    super(player1);
+  } 
+
+  fight(): number {
+    while (this.player1.lifePoints > 0 && this.player2.lifePoints > 0) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
+    }
+    return super.fight();
+  }
+}
+
+export default PVP;
